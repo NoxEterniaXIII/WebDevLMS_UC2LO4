6b8a5bc2d120aac6e987b3400ba26801c9d94ef1
JCDayauon <demonlordex700@gmail.com>
2020-12-30 19:16:37 +0900

    first commit

---
+++ UC2LO4.js
@@ -0,0 +1,49 @@
+// Object
+
+// var Array = new Array ();
+// Array [0] = 8;
+// Array [1] = "Wazzup";
+
+// var R6 = new Object ();
+// R6.maxSpeed = 299;
+// R6.driver = "JC";
+// R6.drive = function()
+// 	{
+// 	console.log("Currently Driving");};
+
+// R6.drive();
+
+// var R1 = {
+// 	maxSpeed: 326, 
+// 	driver: "Dayauon",
+//     drive: function(speed, time)
+//     {
+// 	  console.log(speed * time);
+//         }
+// 	logDriver: function(){
+// 	  console.log("Driver is " + this.driver);
+// 	}
+
+// };
+
+// R1.logDriver();
+// console.log(R1.maxSpeed);
+// R1.drive(100,6);
+
+
+// var myDate = new Date();
+// console.log(myDate);
+
+// var myDate = new Date();
+// console.log(myDate);
+
+// var birthday = new Date (1991, 5, 30, 11, 15, 25);
+// var myPastDate = new Date (1782, 5, 30);
+// var myFutureDate = new Date (2021, 2, 30);
+
+// console.log(birthday);
+// console.log(myPastDate);
+// console.log(myFutureDate);
+
+
+
